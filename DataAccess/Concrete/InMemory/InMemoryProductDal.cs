b2f3549762dbb1e375d659288b4434d3cdbfc041
alperentzn14 <alperentzn14@gmail.com>
2022-01-01 21:17:26 +0300

    Dtos added

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,10 +19,10 @@ namespace DataAccess.Concrete.InMemory
         {
             //oracle,sql,postgresql,mongoDB...
             _products = new List<Product> {
-            new Product{ProductID=1,CategoryID=1,ProductName="Beer",UnitPrice=15,UnitsInStock=20},
-             new Product{ProductID=2,CategoryID=2,ProductName="Beef",UnitPrice=50,UnitsInStock=30},
-              new Product{ProductID=3,CategoryID=2,ProductName="Food",UnitPrice=30,UnitsInStock=50},
-               new Product{ProductID=4,CategoryID=1,ProductName="Water",UnitPrice=5,UnitsInStock=100},
+            new Product{ProductId=1,CategoryId=1,ProductName="Beer",UnitPrice=15,UnitsInStock=20},
+             new Product{ProductId=2,CategoryId=2,ProductName="Beef",UnitPrice=50,UnitsInStock=30},
+              new Product{ProductId=3,CategoryId=2,ProductName="Food",UnitPrice=30,UnitsInStock=50},
+               new Product{ProductId=4,CategoryId=1,ProductName="Water",UnitPrice=5,UnitsInStock=100},
 
             };
         }
@@ -32,7 +33,7 @@ namespace DataAccess.Concrete.InMemory
 
         public void Delete(Product product)
         {
-            Product productToDelete = _products.SingleOrDefault(p=>p.ProductID==product.ProductID);
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
                 _products.Remove(product);
         }
 
@@ -43,16 +44,16 @@ namespace DataAccess.Concrete.InMemory
 
         public void Update(Product product)
         {
-            Product productToUpdate = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
             productToUpdate.ProductName = product.ProductName;
-            productToUpdate.CategoryID = product.CategoryID;
+            productToUpdate.CategoryId = product.CategoryId;
             productToUpdate.UnitPrice = product.UnitPrice;
             productToUpdate.UnitsInStock = product.UnitsInStock;
         }
         public
         List<Product> GetAllByCategory(int categoryId)
         {
-           return _products.Where(p => p.CategoryID == categoryId).ToList();
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
             
         }
 
@@ -62,6 +63,11 @@ namespace DataAccess.Concrete.InMemory
         }
 
         public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
         {
             throw new NotImplementedException();
         }
--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,4 +1,4 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,11 +7,9 @@ namespace Entities.Concrete
 {
     public class Product:IEntity
     {
-        public int ProductID { get; set; }
-
-        public int CategoryID { get; set; }
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
         public string ProductName { get; set; }
-
         public short UnitsInStock  { get; set; }
         public Decimal UnitPrice { get; set; }
 
